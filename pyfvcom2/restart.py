f78fe5dd9a8f1334f5846b41322f901ff8ec9086
James Clark <jcl@pml.ac.uk>
2025-10-22 20:42:41 +0100

    Fix handling of time in write_restart

--- pyfvcom2/restart.py
+++ pyfvcom2/restart.py
@@ -4,7 +4,7 @@ import numpy as np
 from netCDF4 import Dataset
 from datetime import datetime
 import subprocess
-from Typing import Optional
+from typing import Optional
 
 
 def write_restart(
@@ -29,13 +29,21 @@ def write_restart(
         var_list.extend(["time", "Itime", "Times"])
 
     # If overwriting the time variables
-    exclude_vars = " ".join([f"{var}," for var in var_list])[
+    exclude_vars = "".join([f"{var}," for var in var_list])[
         :-1
     ]  # Remove trailing comma
 
     # Use ncks to copy over everything from the template file except the variables listed in the data dict
-    ncks_command = f"ncks -O -x -v {exclude_vars} {template_file_path} {output_path}"
-    subprocess.run(ncks_command, shell=True, check=True)
+    ncks_command = f"ncks --no_crd -O -x -v {exclude_vars} {template_file_path} {output_path}"
+
+    # Capture the output and errors
+    result = subprocess.run(ncks_command, shell=True, check=True)
+    if result.returncode != 0:
+        # Print the error message
+        print(f"Error occurred while running ncks command: {result.stderr}")
+        raise RuntimeError(
+            f"ncks command failed with return code {result.returncode}"
+        )
 
     # Now append the new data to the output file. We will copy the attributes from the template file
     # and then write the new data.
@@ -62,16 +70,27 @@ def write_restart(
                 f'{new_datetime.strftime("%Y-%m-%d")}T00:00:00.000000'
             )
 
-            # Update the time variables in the output dataset
-            output_ds.variables["time"][:] = np.asarray(
-                new_time, dtype=template_ds.variables["time"].datatype
-            )
-            output_ds.variables["Itime"][:] = np.asarray(
-                new_Itime, dtype=template_ds.variables["Itime"].datatype
-            )
-            output_ds.variables["Times"][:] = np.asarray(
-                new_times, dtype=template_ds.variables["Times"].datatype
-            )
+            # Create the three time variables if they do not already exist
+            for time_var_name in ["time", "Itime", "Times"]:
+                template_var = template_ds.variables[time_var_name]
+                output_ds.createVariable(
+                    time_var_name,
+                    template_var.datatype,
+                    template_var.dimensions,
+                    zlib=True,
+                    complevel=4,
+                )
+
+                # Copy variable attributes
+                for attr_name in template_var.ncattrs():
+                    output_ds.variables[time_var_name].setncattr(
+                        attr_name, template_var.getncattr(attr_name)
+                    )
+
+                # Update the time variables in the output dataset
+                output_ds.variables[time_var_name][:] = np.asarray(
+                    new_time, dtype=template_ds.variables[time_var_name].datatype
+                )
 
         # Now write the data variables
         for var_name, var_data in data.items():
@@ -94,4 +113,4 @@ def write_restart(
                 var_data = var_data.astype(template_var.datatype)
 
             # Write the data
-            output_var[:] = var_data
+            output_var[0,:] = var_data
